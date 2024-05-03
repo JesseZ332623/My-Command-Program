cec0480a5b9d672629471028de8ca30e3225e210
JesseGoogle <zhj3191955858@gmail.com>
2024-05-03 13:36:51 +0800

    一些全局变量与宏的声明

--- fileCopy/include/copyDefs.h
+++ fileCopy/include/copyDefs.h
@@ -1,11 +1,12 @@
 #ifndef _COPY_DEFS_H_
 #define _COPY_DEFS_H_
 
-#include <MyLib/cinCheck.h>
+#include <MyLib/myLogerDef.h>
 
 #include <fstream>
 #include <cstring>
 #include <chrono>
+#include <algorithm>
 
 /**
  *  @brief 计算一个函数的执行时间，精确到毫秒。
@@ -22,11 +23,13 @@
  * @brief 不同操作系统对路径斜杠的使用都不一样，
  *        Windows 用反斜杠 Linux 用正斜杠
 */
-#define WINDOWS_X86 true 
-#define LINUX_UBUNTU false 
+#define WINDOWS_X86 false 
+#define LINUX_UBUNTU true 
 
 /*软件版本号*/
 const std::string SOFTWARE_VERSION = "1.0.0";
+
+static const char * clearCommand = (WINDOWS_X86 ? "cls" : "clear");
 
 /*通过系统确定使用正反斜杠*/
 const constexpr char OS_SPECIFIC_SLASH = (WINDOWS_X86 ? '\\' : '/');
