dfca4ff8e8ff33ec83dc9408af42477b302a6a76
浩进 朱 <3191955858@qq.com>
2024-07-22 22:58:50 +0800

    管理员类测试用例

---
+++ meetingOrder/test/test_administrator.cpp
@@ -0,0 +1,10 @@
+#include "../include/administrator.h"
+
+int main(int argc, char const *argv[])
+{
+    Administrator testAdministrator;
+
+    testAdministrator.showAllRoomState();
+    
+    return EXIT_SUCCESS;
+}
