a6ebaccaef04476de0e7fe0ada6722ad634c871a
浩进 朱 <3191955858@qq.com>
2024-07-22 22:58:12 +0800

    会议室预约系统项目的通用数据，头文件等

--- meetingOrder/include/defs.h
+++ meetingOrder/include/defs.h
@@ -2,5 +2,9 @@
 #define __DEFS_H_
 
 #include <MyLib/myLib.h>
+#include <fstream>
+
+static const std::string DATAFILE_PATH = "../data/meet_room_info.dat";  // 数据文件路径
+static const std::string ADMINISTRATOR_INFO_PATH = "../data/administratorInfo.txt";
 
 #endif // __DEFS_H_
