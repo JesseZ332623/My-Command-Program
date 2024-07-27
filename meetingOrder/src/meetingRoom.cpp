4e740151b81e1076947cdaad3fcd0e5627f0e5d2
浩进 朱 <3191955858@qq.com>
2024-07-27 10:54:49 +0800

    会议室类成员方法及接口的实现

--- meetingOrder/src/meetingRoom.cpp
+++ meetingOrder/src/meetingRoom.cpp
@@ -9,9 +9,9 @@ void Meeting_Room::writeToFile(std::ofstream & __outputData)
         throw std::runtime_error("Can not open: " + DATAFILE_PATH + '\n');
     }
 
-    std::size_t roomNoLen = this->roomNo.size();
-    std::size_t introLen  = this->introduce.size();
-    std::size_t objectBytes = this->getObjectBytes();
+    std::size_t roomNoLen = this->roomNo.size();        // 拿到会议室号字符串的长度
+    std::size_t introLen  = this->introduce.size();     // 拿到会议室介绍字符串的长度
+    std::size_t objectBytes = this->getObjectBytes();   // 拿到单个对象序列化后的字节数
 
     // 写入整个类数据序列化后的字节数。
     __outputData.write(reinterpret_cast<char *>(&objectBytes), sizeof(std::size_t));
@@ -30,27 +30,31 @@ void Meeting_Room::writeToFile(std::ofstream & __outputData)
     __outputData.write(reinterpret_cast<char *>(&introLen), sizeof(std::size_t));
     __outputData.write(this->introduce.c_str(), this->introduce.size());
 
-    __outputData.close();
+    __outputData.close();      // 关闭文件流
 }
 
 void Meeting_Room::show(void) const noexcept
 {
     using namespace MyLib::MyLoger;
+    using namespace MyLib::MyDelay;
 
     printSplitLine(45, '-', std::cout);
 
     NOTIFY_LOG("Meeting Room No. ["); CORRECT_LOG(this->roomNo); NOTIFY_LOG("]\n");
+    delay(15);
 
     NOTIFY_LOG("Room can accommodate: "); 
     CORRECT_LOG(std::to_string(this->roomContain) + " person.\n");
+    delay(15);
 
     NOTIFY_LOG("Meeting room media state: "); 
-    CORRECT_LOG(std::string((this->hasMidia) ? "true" : "false") + "\n\n");
+    if (this->hasMidia) { CORRECT_LOG("true\n\n"); }
+    else { std::cout << ERROR << "false" << ORIGINAL << "\n\n"; }
+    delay(15);
 
     NOTIFY_LOG("Meeting room introduce: \n");
-    ORIGINAL_LOG(this->introduce + '\n');
-
-    printSplitLine(45, '-', std::cout);
+    ORIGINAL_LOG(this->introduce + "\n\n");
+    delay(15);
 }
 
 void Meeting_Room::inputInfo(void)
@@ -65,6 +69,7 @@ void Meeting_Room::inputInfo(void)
         ""
     };
 
+    // 输入会议号
     while (true)
     {
         NOTIFY_LOG("Enter Meet Room No. (8 character limit): ");
@@ -75,17 +80,23 @@ void Meeting_Room::inputInfo(void)
             ERROR_LOG("Empty room number! Please enter again!\n");
             continue;
         }
+        if (checkQuit(this->roomNo)) {
+            CORRECT_LOG("BACK TO MENU\n"); return;
+        }
         break;
     }
 
+    // 输入会议室所容纳的人数，并作检查
     istreamInputAndCheck(std::cin, this->roomContain, peopleAccomodate);
 
+    // 会议室是否支持多媒体
     char hasMedia;
     NOTIFY_LOG("This meeting room has media? (Y/N): ");
     std::cin >> hasMedia;
     eatLine();
     this->hasMidia = (hasMedia) ? true : false;
 
+    // 输入会议室介绍
     while (true)
     {
         NOTIFY_LOG("Enter meeting room introduce: ");
@@ -98,8 +109,6 @@ void Meeting_Room::inputInfo(void)
         }
         break;
     }
-
-    //this->writeToFile();
 }
 
 std::ostream & operator<<(std::ostream & __os, const Meeting_Room & __info)
