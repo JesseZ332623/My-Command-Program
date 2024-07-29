dff8f2526cf1472cbb7349818e01044be006e55b
浩进 朱 <3191955858@qq.com>
2024-07-29 08:55:25 +0800

    管理员操作的主逻辑

--- meetingOrder/src/administrator_main.cpp
+++ meetingOrder/src/administrator_main.cpp
@@ -3,6 +3,7 @@
 enum AdministratorChoice
 {
     ADD_NEW_MEETIMNG_ROOM = 1,
+    ADD_NEW_MEETIMNG_ROOM_BETCH,
     MODIFY_METTING_ROOM_DATA,
     SEARCH_ONE_ROOM_STATE,
     DELETE_ONE_ROOM_STATE,
@@ -11,7 +12,7 @@ enum AdministratorChoice
     SHOW_OPERATOR_MENU,
     RESET_ACCOUNT,
     LOG_OUT,
-    EXIT_SYSYTEM
+    EXIT_SYSYTEM = 0
 };
 
 int main(int argc, char const *argv[])
@@ -32,7 +33,8 @@ int main(int argc, char const *argv[])
     {
         "Enter your choice: ",
         "Invalid input! Enter number please!\n",
-        "Out of range input! Enter number (1 ~ 6) please!\n"
+        "Out of range input! Enter number (0 ~ " + 
+        std::to_string(LOG_OUT) + ") please!\n"
     };
     unsigned short administratorChoice;       // 用户的选择
 
@@ -43,29 +45,38 @@ int main(int argc, char const *argv[])
         administrator.showOperatorMenu();
         istreamInputAndCheck(
             std::cin, administratorChoice, choiceStrings,
-            [] (const unsigned short __choice) { return (__choice >= 1 && __choice <= EXIT_SYSYTEM); }
+            [] (const unsigned short __choice) { return (__choice >= 0 && __choice <= LOG_OUT); }
         );
         eatLine();
 
         switch (administratorChoice)
         {
             case ADD_NEW_MEETIMNG_ROOM:
+                system("cls");
                 administrator.addNewMeetingRoom();
                 break;
 
+            case ADD_NEW_MEETIMNG_ROOM_BETCH:
+                administrator.betchAddNewMettingRoom();
+                break;
+
             case MODIFY_METTING_ROOM_DATA:
+                system("cls");
                 administrator.modifyMeetRoomData();
                 break;
             
             case SEARCH_ONE_ROOM_STATE:
+                system("cls");
                 administrator.searchOneRoomState();
                 break;
 
             case DELETE_ONE_ROOM_STATE:
+                system("cls");
                 administrator.deleteMeetRoom();
                 break;
 
             case DELETE_ALL_ROOM_STATE:
+                system("cls");
                 administrator.deleteAllMeetingRoom();
                 break;
 
@@ -77,16 +88,18 @@ int main(int argc, char const *argv[])
             case SHOW_OPERATOR_MENU:
                 system("cls");
                 break;
+
+            case RESET_ACCOUNT:
+                system("cls");
+                administrator.resetAccount();
+                break;
             
             case LOG_OUT:
                 if (administrator.logout()) { administrator.login(); }
                 break;
-
-            case RESET_ACCOUNT:
-                administrator.resetAccount();
-                break;
             
             case EXIT_SYSYTEM:
+                system("cls");
                 CORRECT_LOG("Have a good time! Bye!\n");
                 DONE;
                 exit(EXIT_SUCCESS);
