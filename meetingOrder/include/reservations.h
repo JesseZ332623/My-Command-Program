0f823d629ee06c561c665e9f10e935ace10062a6
浩进 朱 <3191955858@qq.com>
2024-08-01 09:08:08 +0800

    预约人员类的声明

--- meetingOrder/include/reservations.h
+++ meetingOrder/include/reservations.h
@@ -2,30 +2,26 @@
 #define __RESERVATIONS_H_
 
 #include "./defs.h"
-#include "meetingRoom.h"
+#include "./meetingRoom.h"
+#include "./cryption.h"
 
+// 一个预约人员最大可预约的会议室
 const std::size_t RESERVATION_LIMIT = 3;
 
 class Reservations
 {
-    public:
+    private:
         struct ReservationInfo
         {
             std::string name;           // 预约人员账号
             std::string password;
 
-            /*
-                用于仿射密码加密的结构体，
-                a 和 b 作为一元函数f(x) = ax + b 的常数。
-            */
-            struct Affine { int a; int b; };
+            Cryption cryption;
 
             /**
-             * @brief 对 password 进行加密，使用仿射加密。
-             * 
-             * @return Affine 返回仿射结构体
+             * @brief 输入这个类的数据去初始化这个类
              */
-            Affine encryption(void);
+            void getInput(void);
             
             /**
              * @brief 将单个加密后的预约人员对象数据写入文件
@@ -35,41 +31,42 @@ class Reservations
             void writeReservationDataToFile(std::ofstream & __writeStream);
 
             /**
+             * @brief 读取数据文件填充到结构体中
+             * 
+             * @param __readStream      读文件流
+             */
+            void readDataToFill(std::ifstream & __readStream);
+
+            /**
              * @brief 清除单个预约人员账户的信息
              */
             void clearInfo(void) { this->name.clear(); this->password.clear(); }
 
-            void show(void) { printf("{%s, %s}\n", this->name.c_str(), this->password.c_str()); }
-        };
+            void show(void) { printf("\n{%s}\n", this->password.c_str()); }
 
-        struct MeetRoomState 
-        {
-            Meeting_Room meetingRoom;
-            bool hasOccupaied;
-
-            void showState(void) {}
+            friend bool operator==(const ReservationInfo & __a, const ReservationInfo & __b)
+            { return (__a.name == __b.name) && (__a.password == __b.password); }
         };
         
-        std::size_t   currentReservationCount;
+        ReservationInfo reservationInfo;    // 预约人员账号对象
 
-        std::vector<MeetRoomState> roomRecord;
+        std::size_t     currentReservationCount;
 
         std::ifstream readStream;
         std::ofstream writeStream;
 
-        /**
-         * @brief 加载数据文件，
-         *        保存到动态数组 roomRecord 之中，作为构建函数的辅助函数。
-         * 
-         * @param __path                数据文件路径
-         */
-        void loadRoomDataFile(const std::string & __path);
+        void loadAccountInfo(void);
 
     public:
         /**
-         * @brief Construct a new Reservations object
+         * @brief 默认构造函数，如果是新用户则要求注册
         */
         Reservations(void);
+
+        /**
+         * @brief 预约人员登录
+         */
+        void login(void);
 };
 
 #endif // __RESERVATIONS_H_
